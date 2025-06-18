5f4f2db07610cf654a834be04ab05440c78b8aad
Harshit Shukla <harshitshukla289@gmail.com>
2025-06-18 21:05:35 +0530

    Update ProtectedRoute.jsx

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
-
-import { Navigate } from 'react-router-dom';
-import { isAuthenticated } from '../utils/auth';
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
