f3e0d8c7265c20d4769335b8aba81f8264dc0688
Harshit Shukla <harshitshukla289@gmail.com>
2025-06-19 10:38:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use(cors({
    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
+app.options("*", cors());
 
 // Routes
 app.use('/api/auth', authRoutes);
